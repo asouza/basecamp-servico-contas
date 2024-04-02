3ecf492d02dba971b809232a0ba1bcbb285d856a
Alberto <alberto.tavares@zup.com.br>
2024-04-02 16:56:52 -0300

    codigo no intellij

--- src/test/java/com/deveficiente/basecamp/contas/NovaContaControllerTest.java
+++ src/test/java/com/deveficiente/basecamp/contas/NovaContaControllerTest.java
@@ -25,55 +25,56 @@ import jakarta.transaction.Transactional;
 @SpringBootTest
 @AutoConfigureMockMvc
 class NovaContaControllerTest {
-	
-	@Autowired
-	private CustomMockMvc customMockMvc;
-	@MockBean
-	private PessoaUsuariaClient pessoaUsuariaClient;
 
-	@Test
-	@DisplayName("Deve criar uma nova conta quando a pessoa usuaria ainda não está numa conta como owner primaria")
-	@Transactional
-	void test1() throws Exception {
-		HashMap<String, Object> json = new HashMap<>();
-		json.put("nome", "nome da conta");
-		json.put("novaContaOwnerRequest", Map.of("nome","nome","email","email@email.com","senha","123456"));
-		
-		NovaContaOwnerRequest ownerRequest = new NovaContaOwnerRequest("nome", "email@email.com", "123456");
-		NovaPessoaUsuarioRequest pessoaUsuariaRequest = new NovaPessoaUsuarioRequest(ownerRequest);
-		UUID idGlobalOwner = UUID.randomUUID();
-		Mockito
-			.when(pessoaUsuariaClient.criaPessoaUsuaria(pessoaUsuariaRequest))
-			.thenReturn(idGlobalOwner);
-		
-		ResultActions resultado = customMockMvc.post("/api/contas/v1",json);
-		
-		resultado.andExpect(status().isOk());			
-	}
-	
-	@Test
-	@DisplayName("Deve direcionar para um endereco de adicao de contas quando a pessoa usuaria já é owner primaria de uma conta")
-	@Transactional
-	void test2() throws Exception {
-		HashMap<String, Object> json = new HashMap<>();
-		json.put("nome", "nome da conta");
-		json.put("novaContaOwnerRequest", Map.of("nome","nome","email","email@email.com","senha","123456"));
-		
-		NovaContaOwnerRequest ownerRequest = new NovaContaOwnerRequest("nome", "email@email.com", "123456");
-		NovaPessoaUsuarioRequest pessoaUsuariaRequest = new NovaPessoaUsuarioRequest(ownerRequest);
-		UUID idGlobalOwner = UUID.randomUUID();
-		Mockito
-			.when(pessoaUsuariaClient.criaPessoaUsuaria(pessoaUsuariaRequest))
-			.thenReturn(idGlobalOwner);	
-		
-		//registrando a primeira vez
-		customMockMvc.post("/api/contas/v1",json);
-		
-		ResultActions resultado = customMockMvc.post("/api/contas/v1",json);
-		
-		resultado.andExpect(status().isFound());
-		
-		
-	}	
+    @Autowired
+    private CustomMockMvc customMockMvc;
+    @MockBean
+    private PessoaUsuariaClient pessoaUsuariaClient;
+
+    @Test
+    @DisplayName("Deve criar uma nova conta quando a pessoa usuaria ainda não está numa conta como owner primaria")
+    @Transactional
+    void test1() throws Exception {
+        HashMap<String, Object> json = new HashMap<>();
+        json.put("nome", "nome da conta");
+        json.put("novaContaOwnerRequest", Map.of("nome", "nome", "email", "email@email.com", "senha", "123456"));
+
+        NovaContaOwnerRequest ownerRequest = new NovaContaOwnerRequest("nome", "email@email.com", "123456");
+        NovaPessoaUsuarioRequest pessoaUsuariaRequest = new NovaPessoaUsuarioRequest(ownerRequest);
+        UUID idGlobalOwner = UUID.randomUUID();
+        Mockito
+                .when(pessoaUsuariaClient.criaPessoaUsuaria(pessoaUsuariaRequest))
+                .thenReturn(idGlobalOwner);
+
+        ResultActions resultado = customMockMvc.post("/api/contas/v1", json);
+
+        resultado.andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Deve direcionar para um endereco de adicao de contas quando a pessoa usuaria já é owner primaria de" +
+            " uma conta")
+    @Transactional
+    void test2() throws Exception {
+        HashMap<String, Object> json = new HashMap<>();
+        json.put("nome", "nome da conta");
+        json.put("novaContaOwnerRequest", Map.of("nome", "nome", "email", "email@email.com", "senha", "123456"));
+
+        NovaContaOwnerRequest ownerRequest = new NovaContaOwnerRequest("nome", "email@email.com", "123456");
+        NovaPessoaUsuarioRequest pessoaUsuariaRequest = new NovaPessoaUsuarioRequest(ownerRequest);
+        UUID idGlobalOwner = UUID.randomUUID();
+        Mockito
+                .when(pessoaUsuariaClient.criaPessoaUsuaria(pessoaUsuariaRequest))
+                .thenReturn(idGlobalOwner);
+
+        //registrando a primeira vez
+        customMockMvc.post("/api/contas/v1", json);
+
+        ResultActions resultado = customMockMvc.post("/api/contas/v1", json);
+
+        resultado.andExpect(status().isFound());
+
+
+    }
 
 }
